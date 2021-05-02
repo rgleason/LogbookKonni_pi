f4314f506448799525d0597fe03fb7d60de98a2a
Rick Gleason <fcgleason@tgp-architects.com>
2021-05-02 11:46:05 -0400

    v1.4.13.17 + fix select but not help

--- src/LogbookDialog.cpp
+++ src/LogbookDialog.cpp
@@ -3744,7 +3744,7 @@ Backup Logbook(*.txt)|*.txt" );
     wxString s = wxFileName::GetPathSeparator();
     // help_locn = *GetpSharedDataLocation() + _T( "plugins" ) + s + _T( "logbookkonni_pi" ) + s + _T( "data" ) + s;
     // help_locn =  GetPluginDataDir("logbookkonni_pi") + _T("/data/");
-    help_locn =  GetPluginDataDir("logbookkonni_pi");
+    help_locn =  GetPluginDataDir("logbookkonni_pi") + _T("/");
 	image_locn = help_locn + _T( "Images" ) + s;
 
     lastRowSelectedRepairs = 0;
