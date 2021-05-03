975f2ee1590651f4977de4947a6a1dbb837bd44a
Rick Gleason <fcgleason@tgp-architects.com>
2021-05-02 21:59:57 -0400

    v1.4.13.18 + fix help build all

--- src/LogbookDialog.cpp
+++ src/LogbookDialog.cpp
@@ -3743,8 +3743,7 @@ Backup Logbook(*.txt)|*.txt" );
 
     wxString s = wxFileName::GetPathSeparator();
     // help_locn = *GetpSharedDataLocation() + _T( "plugins" ) + s + _T( "logbookkonni_pi" ) + s + _T( "data" ) + s;
-    // help_locn =  GetPluginDataDir("logbookkonni_pi") + _T("/data/");
-    help_locn =  GetPluginDataDir("logbookkonni_pi") + _T("/");
+    help_locn =  GetPluginDataDir("logbookkonni_pi") + _T("/data/");
 	image_locn = help_locn + _T( "Images" ) + s;
 
     lastRowSelectedRepairs = 0;
