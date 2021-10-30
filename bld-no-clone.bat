6864882e5d1982775b5d684570830e91d42e78d1
Rick Gleason <fcgleason@tgp-architects.com>
2021-10-30 13:35:43 -0400

    1.4.22.0 fix summary

--- bld-no-clone.bat
+++ bld-no-clone.bat
@@ -2,19 +2,19 @@ REM As described here:
 REM wiki](https://github.com/Rasbats/managed_plugins/wiki/Local-Build)
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-set WXWIN=C:\users\fcgle\source\wxWidgets
+set WXWIN=C:\Users\fcgle\source\wxWidgets
 set wxWidgets_ROOT_DIR=%WXWIN%
 set wxWidgets_LIB_DIR=%WXWIN%\lib\vc_dll
 SET PATH=%PATH%;%WXWIN%;%wxWidgets_LIB_DIR%;C:\Program Files (x86)\Poedit\Gettexttools\bin;
 set PATH=C:\Program Files\Cmake\bin;%PATH%
 
 REM Example Only. Change shipdriver_pi to match the repo being used.
-cd shipdriver_pi
+REM cd shipdriver_pi
 
 mkdir  build
 cd build
 cmake -T v141_xp ..
-cmake -G "Visual Studio 15 2017" --config RelWithDebInfo  ..
-cmake --build . --target tarball --config RelWithDebInfo
+cmake -G "Visual Studio 15 2017" --config RelWithDebInfo  ..  
+cmake --build . --target tarball --config RelWithDebInfo >output.txt
 
 cmd /k
